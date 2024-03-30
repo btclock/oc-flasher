e670b3f71f39071e0b01f3589e3d84263b0377a4
Djuri Baars <dsbaars@users.noreply.github.com>
2024-03-30 15:17:52 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import {
+		Collapse,
+		Dropdown,
+		DropdownItem,
+		DropdownMenu,
+		DropdownToggle,
+		Nav,
+		NavItem,
+		NavLink,
+		Navbar,
+		NavbarBrand
+	} from '@sveltestrap/sveltestrap';
+
+	import { page } from '$app/stores';
+	import { locale, locales, isLoading } from 'svelte-i18n';
+
+	export const setLocale = (lang: string) => () => {
+		locale.set(lang);
+		localStorage.setItem('locale', lang);
+	};
+
+	export const getFlagEmoji = (languageCode: string): string | null => {
+		const flagMap: { [key: string]: string } = {
+			en: '🇬🇧', // English flag emoji
+			nl: '🇳🇱', // Dutch flag emoji
+			es: '🇪🇸' // Spanish flag emoji
+		};
+
+		// Convert the language code to lowercase for case-insensitive matching
+		const lowercaseCode = languageCode.toLowerCase();
+
+		// Check if the language code is in the flagMap
+		if (Object.prototype.hasOwnProperty.call(flagMap, lowercaseCode)) {
+			return flagMap[lowercaseCode];
+		} else {
+			// Return null for unsupported language codes
+			return null;
+		}
+	};
+
+	let languageNames = {};
+
+	locale.subscribe(() => {
+		let newLanguageNames = new Intl.DisplayNames([$locale], { type: 'language' });
+
+		for (let l: string of $locales) {
+			languageNames[l] = newLanguageNames.of(l);
+		}
+	});
+</script>
+
+<Navbar expand="md">
+	<NavbarBrand>
+		<img src="/assets/logo-dark.svg" alt="OrangeClock" class="darkModeLogo" />
+		<img src="/assets/logo.svg" alt="OrangeClock" class="lightModeLogo" />
+	</NavbarBrand>
+	<Collapse navbar expand="md">
+		<Nav class="me-auto" navbar>
+			<NavItem>
+				WebFlasher
+			</NavItem>
+		</Nav>
+	</Collapse>
+</Navbar>
+
+<!-- +layout.svelte -->
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	//	import { PUBLIC_BASE_URL } from '$lib/config';
+
+	import {
+		Container,
+		Col,
+		Card,
+		CardHeader,
+		CardTitle,
+		CardSubtitle,
+		CardFooter,
+		CardBody,
+		Button,
+		CardText,
+		Row
+	} from '@sveltestrap/sveltestrap';
+	// import Settings from './Settings.svelte';
+	// import Status from './Status.svelte';
+	// import Control from './Control.svelte';
+	import { onDestroy, onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+</script>
+
+<svelte:head>
+	<title>OrangeBTClock</title>
+	<script
+		type="module"
+		src="https://unpkg.com/esp-web-tools@10/dist/web/install-button.js?module"
+	></script>
+</svelte:head>
+
+<Container>
+	<Row>
+		<Col>
+			<Card>
+				<CardHeader>
+					<CardTitle>Wemos S2/S3 mini</CardTitle>
+				</CardHeader>
+				<CardBody>
+                    <esp-web-install-button manifest="/manifests/epd2_13.json">
+                        <span slot="activate"><Button color="primary">2.13 inch</Button></span>
+                    </esp-web-install-button>
+					<esp-web-install-button manifest="/manifests/epd2_9.json">
+                        <span slot="activate"><Button color="secondary">2.9 inch</Button></span>
+                    </esp-web-install-button>
+				</CardBody>
+			</Card>
+		</Col>
+		<Col>
+			<Card>
+				<CardHeader>
+					<CardTitle>OrangeClock PCB</CardTitle>
+				</CardHeader>
+				<CardBody>
+                    <esp-web-install-button manifest="/manifests/oc_pcb.json">
+                        <span slot="activate"><Button color="secondary">2.9 inch</Button></span>
+                    </esp-web-install-button>
+                </CardBody>
+			</Card>
+		</Col>
+	</Row>
+    <Row>
+        <Col>
+            <h1>How it works</h1>
+            <p>ESP Web Tools works by using Web Serial and a manifest which describes the firmware. ESP Web Tools detects the chipset of the connected ESP device and automatically selects the right firmware variant from the manifest.</p>
+            <p>Web Serial is available in Google Chrome and Microsoft Edge browsers. Android support should be possible but has not been implemented yet.</p>
+        </Col>
+    </Row>
+</Container>
